01129a81e799b817ffc2a6973d2d047bd5c4f89f
Thaer <issathaer7@gmail.com>
2026-05-13 17:05:33 +0200

    Add database configuration file for PDO connection

---
+++ config/database.php
@@ -0,0 +1,21 @@
+<?php
+
+$host = "localhost";
+$dbname = "task_manager";
+$username = "root";
+$password = "";
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $username,
+        $password
+    );
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "DB OK 🚀";
+
+} catch (PDOException $e) {
+    die("Fehler: " . $e->getMessage());
+}
